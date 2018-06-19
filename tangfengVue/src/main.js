33d287961bbaeb4bbf4960f34d2835e65f090487
zsmgit <m17512052246@163.com>
2018-06-19 15:36:44 +0800

    change main.js

--- tangfengVue/src/main.js
+++ tangfengVue/src/main.js
@@ -13,22 +13,22 @@ if (window.sessionStorage.getItem('userinfo')) {
 }
 
 // router 的全局钩子
-//router.beforeEach((to, from, next) => { // 一个路由匹配到的所有路由记录暴露为一个$route对象（或者是此处 to from 代表的路由钩子)的$route.matched数组
-//if (to.matched.some(record => record.meta.reqireLogin)) { // 判断即将进入的路由是否需要先登录
-//  if (!store.getters.isLogin) {
-//    next({ // 此处是必不可少的 不然路由无法进行下去   可以在next中 进行取消跳转 重定向等
-//      path: '/tfBindMobile',
-//      query: { // 传递参数 登录页以此进行判断是否需要自动跳转
-//        redirect: to.fullPath
-//      }
-//    })
-//  } else {
-//    next()
-//  }
-//} else {
-//  next()
-//}
-//})
+router.beforeEach((to, from, next) => { // 一个路由匹配到的所有路由记录暴露为一个$route对象（或者是此处 to from 代表的路由钩子)的$route.matched数组
+if (to.matched.some(record => record.meta.reqireLogin)) { // 判断即将进入的路由是否需要先登录
+    if (!store.getters.isLogin) {
+      next({ // 此处是必不可少的 不然路由无法进行下去   可以在next中 进行取消跳转 重定向等
+        path: '/',
+        query: { // 传递参数 登录页以此进行判断是否需要自动跳转
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      next()
+    }
+} else {
+    next()
+}
+})
 
 /* eslint-disable no-new */
 new Vue({
